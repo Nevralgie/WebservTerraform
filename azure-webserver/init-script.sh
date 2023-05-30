b70d0401d5c3796fd46245358d01aa71e29cc9fc
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-05-30 09:59:20 +0200

    Add files via upload

---
+++ azure-webserver/init-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#!/bin/bash
+
+#Installing Docker
+sudo apt update && sudo apt upgrade -y
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER
+#Creating container
+sudo docker run -d -p 80:80 -h nginxtfb13 nevii/tfb13nginx-arap22-ren-t:latest
