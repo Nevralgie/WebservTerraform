cae598a2e6e423f20a87617fdd72cfa2b9ef842e
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-05-31 18:57:07 +0200

    Update variables.tf

--- ProdEnvironment/variables.tf
+++ ProdEnvironment/variables.tf
@@ -24,3 +24,13 @@ variable "tenant_id" {
    description = "Azure Tenant ID"
    default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
 }
+
+variable "client_id" {
+   description = "Azure Client ID"
+   default = "1244ff47-5233-442c-b8df-b761e220bc23"
+}
+
+variable "client_secret" {
+   description = "Azure Client Secret"
+   default = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
+}
