7d260dbe2d0494c57eb69d792636db2f13c2beec
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-05-31 16:21:45 +0200

    Update init-script.sh

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az login --service-principal --username a1938c45-c0fb-43a5-87be-ecaa9de8b8b8 --password fe88Q~K0_tI0IWkwQp1nzEO1EosYaxBhWQsojcOV --tenant 7349d3b2-951f-41be-877e-d8ccd9f3e73c
-az acr create --resource-group nginx-servertomtest --name tomregistryb13 --sku Basic --admin-enabled true
-az acr build --registry tomregistryb13 --image nginxbrief13 .
-az config set extension.use_dynamic_install=yes_without_prompt
-az containerapp env create --name tfenvtom --resource-group nginx-servertomtest --location francecentral
-az containerapp create --name nginxaz13 --resource-group nginx-servertomtest --environment tfenvtom --image tomregistryb13.azurecr.io/nginxbrief13 --target-port 80 --ingress 'external' --registry-server tomregistryb13.azurecr.io --query properties.configuration.ingress.fqdn
+sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo az login --service-principal --username a1938c45-c0fb-43a5-87be-ecaa9de8b8b8 --password fe88Q~K0_tI0IWkwQp1nzEO1EosYaxBhWQsojcOV --tenant 7349d3b2-951f-41be-877e-d8ccd9f3e73c
+sudo az acr create --resource-group nginx-servertomtest --name tomregistryb13 --sku Basic --admin-enabled true
+sudo az acr build --registry tomregistryb13 --image nginxbrief13 .
+sudo az config set extension.use_dynamic_install=yes_without_prompt
+sudo az containerapp env create --name tfenvtom --resource-group nginx-servertomtest --location francecentral
+sudo az containerapp create --name nginxaz13 --resource-group nginx-servertomtest --environment tfenvtom --image tomregistryb13.azurecr.io/nginxbrief13 --target-port 80 --ingress 'external' --registry-server tomregistryb13.azurecr.io --query properties.configuration.ingress.fqdn
 
 #sudo killall apt 
 #Installing Docker
