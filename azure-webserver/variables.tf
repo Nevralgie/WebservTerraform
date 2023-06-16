8f156d7eed9b6e4c4c3b53ead71849fc2c153a7b
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-06-16 11:38:51 +0200

    Update variables.tf

--- azure-webserver/variables.tf
+++ azure-webserver/variables.tf
@@ -24,11 +24,11 @@ variable "instance_size" {
    default = "Standard_F2"
 }
 
-#variable "location" {
-   #type = string
-   #description = "Region"
-   #default = "France Central"
-#}
+variable "location" {
+   type = string
+   description = "Region"
+   default = "France Central"
+}
 
 variable "environment" {
    type = string
