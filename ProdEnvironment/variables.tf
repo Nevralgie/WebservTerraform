4ac608b640c811dbfff90347af1fc31f37a5d3af
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-09-18 10:06:02 +0200

    Update variables.tf

--- ProdEnvironment/variables.tf
+++ ProdEnvironment/variables.tf
@@ -18,19 +18,19 @@ variable "environment" {
 
 variable "subscription_id" {
    description = "Azure subscription"
-   default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+   default = "<YOUR_SUBSCRIPTION>"
 }
 variable "tenant_id" {
    description = "Azure Tenant ID"
-   default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
+   default = "<YOUR_TENANT_ID>"
 }
 
 variable "client_id" {
    description = "Azure Client ID"
-   default = "1244ff47-5233-442c-b8df-b761e220bc23"
+   default = "<YOUR_CLIENT_ID>"
 }
 
 variable "client_secret" {
    description = "Azure Client Secret"
-   default = "YtY8Q~Q6kLoKDGGGEn3lGlILr--HZ5EsbJwOGba-"
+   default = "<YOUR_CLIENT_SECRET>"
 }
